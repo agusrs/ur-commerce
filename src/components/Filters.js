6d4936c4004af45d98fb3d99f488fcf10fb7dc36
agusrs <39487421+agusrs@users.noreply.github.com>
2022-01-20 00:17:58 -0300

    Finish filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,40 +1,48 @@
-import { FormControl, FormLabel, Grid, InputLabel, MenuItem, Select } from '@mui/material'
+import { Checkbox, FormControlLabel, FormLabel, Grid, MenuItem, Select, Stack, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
 import React, { useState } from 'react'
 
-const Filters = () => {
-    const [orderBy, setOrderBy] = useState(7)
-
-    const handleChangeOrderBy = (event) => {
-        setOrderBy(event.target.value);
-    }
+const Filters = ({ categories, sizes, onChangeSize, orderBy, handleChangeOrderBy }) => {
 
     return (
         <Grid container direction='column' marginTop={2} spacing={2} >
             <Grid item>
-                    <FormLabel id="demo-simple-select-label">Ordenar por:</FormLabel>
-                    <Select
-                        labelId="order-by-select-label"
-                        id="order-by-select"
-                        value={orderBy}
-                        label="Ordenar por:"
-                        sx={{width: 150}}
-                        variant='standard'
-                        onChange={handleChangeOrderBy}
-                    >
-                        <MenuItem value={1}>Precio: menor a mayor</MenuItem>
-                        <MenuItem value={2}>Precio: mayor a menor</MenuItem>
-                        <MenuItem value={3}>A - Z</MenuItem>
-                        <MenuItem value={4}>Z - A</MenuItem>
-                        <MenuItem value={5}>Más antiguo al más reciente</MenuItem>
-                        <MenuItem value={6}>Más reciente al más antiguo</MenuItem>
-                        <MenuItem value={7}>Más vendidos</MenuItem>
-                    </Select>
+                <FormLabel id="demo-simple-select-label">Ordenar por:</FormLabel>
+                <Select
+                    labelId="order-by-select-label"
+                    id="order-by-select"
+                    value={orderBy}
+                    label="Ordenar por:"
+                    sx={{ width: 150 }}
+                    variant='standard'
+                    onChange={handleChangeOrderBy}
+                >
+                    <MenuItem value={1}>Precio: menor a mayor</MenuItem>
+                    <MenuItem value={2}>Precio: mayor a menor</MenuItem>
+                    <MenuItem value={3}>A - Z</MenuItem>
+                    <MenuItem value={4}>Z - A</MenuItem>
+                    <MenuItem value={5}>Más antiguo al más reciente</MenuItem>
+                    <MenuItem value={6}>Más reciente al más antiguo</MenuItem>
+                    <MenuItem value={7}>Más vendidos</MenuItem>
+                </Select>
             </Grid>
             <Grid item>
-                <FormLabel>Categorías</FormLabel>
+                {categories.length > 1 &&
+                    <Stack spacing={1} >
+                        <FormLabel>Categorías</FormLabel>
+                        {categories.map(c => (
+                            <Typography key={c} component={Link} style={{ color: 'inherit', textDecoration: 'inherit' }} sx={{ ':hover': { textDecoration: 'underline!important' } }} to={`/${c}`} >{c.toUpperCase()}</Typography>
+                        ))}
+                    </Stack>
+                }
             </Grid>
             <Grid item>
+                <Stack>
                 <FormLabel>Talle</FormLabel>
+                {sizes.map(s => (
+                    s.value > 0 && <FormControlLabel key={s.name} control={<Checkbox onChange={(e) => onChangeSize(e, s.name)} />} label={s.name.toUpperCase() + ` (${s.value})`} />
+                ))}
+                </Stack>
             </Grid>
         </Grid>
     )
--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -13,11 +13,48 @@ import Filters from '../Filters'
 const Products = () => {
 
     const [products, setProducts] = useState([])
+    const [filteredProducts, setFilteredProducts] = useState([])
+    const [orderBy, setOrderBy] = useState(7)
     let urlProduct = useParams()
 
     useEffect(() => {
         getProducts(urlProduct)
     }, [urlProduct])
+
+    useEffect(() => {
+        setFilteredProducts(products)
+    }, [products])
+
+    useEffect(() => {
+        let sortedProducts = [...filteredProducts]
+        switch (orderBy) {
+            case 1: //menor precio
+                sortedProducts.sort((a,b) => (a.price > b.price) ? 1 : ((b.price > a.price) ? -1 : 0))
+                break;
+            case 2: //mayor precio
+                sortedProducts.sort((a,b) => (a.price < b.price) ? 1 : ((b.price < a.price) ? -1 : 0))
+                break;
+            case 3: //a-z
+                sortedProducts.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+                break;
+            case 4: //z-a
+                sortedProducts.sort((a,b) => (a.name < b.name) ? 1 : ((b.name < a.name) ? -1 : 0))
+                break;
+            case 5: //mas antiguo
+                
+                break;
+            case 6://mas reciente
+                
+                break;
+            case 7://mas vendidos
+                
+                break;
+            default:
+                break;
+        }
+        setFilteredProducts(sortedProducts)
+    }, [orderBy]);
+
 
     const getProducts = (urlProduct) => {
         api.get(`?${urlProduct.category ? 'category=' + urlProduct.category + '&' : ''}_limit=20`)
@@ -25,8 +62,34 @@ const Products = () => {
             setProducts(res.data)
         })
         .catch((err) => {
-            
+
         })
+    }
+
+    const getSizes = () => {
+        let keys = []
+        filteredProducts.map(p => {
+            keys = [...keys, ...Object.keys(p.stock)];
+        })
+        keys = [...new Set(keys)];
+        keys = keys.map(k => {
+            return { name: k, value: filteredProducts.reduce((total, p) => p.stock[k] ? 1 + total : total, 0) }
+        })
+        return keys
+    }
+
+    const onChangeSize = (event, size) => {
+        let newProducts = [...filteredProducts]
+        if (event.target.checked) {
+            newProducts = newProducts.filter(p => p.stock[size] > 0)
+        } else {
+            newProducts = [...newProducts, ...products.filter(p => !p.stock[size] || p.stock[size] === 0)];
+        }
+        setFilteredProducts(newProducts)
+    }
+
+    const handleChangeOrderBy = (event) => {
+        setOrderBy(event.target.value);
     }
 
     return (
@@ -43,10 +106,10 @@ const Products = () => {
                 <Grid item >
                     <Grid container spacing={2} direction='row'>
                         <Grid item xs={2} >
-                            <Filters />
+                            <Filters categories={[... new Set(products.map(p => p.category))]} sizes={getSizes()} onChangeSize={onChangeSize} handleChangeOrderBy={handleChangeOrderBy} orderBy={orderBy} />
                         </Grid>
                         <Grid item xs={10} >
-                            <ProductList products={products} />
+                            <ProductList products={filteredProducts} />
                         </Grid>
                     </Grid>
                 </Grid>
